02cb22550d888dded43355e5713b6e784a929735
mai_llj <yashinomi0804@gmail.com>
2023-01-15 16:32:53 +0900

    update: apiの運用に向けた修正

--- main.py
+++ main.py
@@ -356,4 +356,4 @@ def get_icon(user_id=None):
         return ReturnJson.err('内部でエラーが発生しました。')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=80)
